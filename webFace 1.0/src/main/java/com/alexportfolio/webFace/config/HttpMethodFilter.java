b22e5f2bf3f8ecf81c5490f5f2d27acde9d98078
Alex <r210391@outlook.com>
2024-06-28 19:36:21 -0400

    Added exception handling for unsupported http methods

---
+++ webFace 1.0/src/main/java/com/alexportfolio/webFace/config/HttpMethodFilter.java
@@ -0,0 +1,35 @@
+package com.alexportfolio.webFace.config;
+
+import jakarta.persistence.Access;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.naming.AuthenticationException;
+import java.io.IOException;
+
+
+public class HttpMethodFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String httpMethod = request.getMethod();
+        if(!isValidHttpMethod(httpMethod))
+            throw new IllegalArgumentException(httpMethod);
+        filterChain.doFilter(request,response);
+    }
+
+    private boolean isValidHttpMethod(String method) {
+        // Check against standard HTTP methods
+        return "GET".equalsIgnoreCase(method) || "POST".equalsIgnoreCase(method) ||
+                "PUT".equalsIgnoreCase(method) || "DELETE".equalsIgnoreCase(method) ||
+                "PATCH".equalsIgnoreCase(method) || "HEAD".equalsIgnoreCase(method) ||
+                "OPTIONS".equalsIgnoreCase(method) || "TRACE".equalsIgnoreCase(method);
+    }
+}
--- webFace 1.0/src/main/java/com/alexportfolio/webFace/config/SecurityConfig.java
+++ webFace 1.0/src/main/java/com/alexportfolio/webFace/config/SecurityConfig.java
@@ -2,19 +2,14 @@ package com.alexportfolio.webFace.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
@@ -29,13 +24,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .addFilterAt( new HttpMethodFilter(),  BasicAuthenticationFilter.class)
+                .addFilterBefore( new ExceptionHandlerFilter(), HttpMethodFilter.class)
                 .securityMatcher("/**") // Configure HttpSecurity to only be applied to URLs that start       // with /
                 .authorizeRequests(authz ->
                         authz
                                 .requestMatchers(HttpMethod.POST,"/settings").hasRole("ADMIN")
                                 .requestMatchers(HttpMethod.GET,"/settings").hasRole("ADMIN")
                                 .requestMatchers(HttpMethod.POST, "/").hasRole("ADMIN")
-
                                 .anyRequest().permitAll()
                 ).formLogin(form->{
                     form
